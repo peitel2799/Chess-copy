5deec7a9980d987d0a678e2c6aa8898377ef8878
peitel2799 <letiep2799@gmail.com>
2024-05-13 01:25:12 +0700

    Version javafx chua sua cua Phuc

---
+++ src/piece/Queen.java
@@ -0,0 +1,34 @@
+package piece;
+
+import main.GamePanel;
+
+public class Queen extends Piece{
+
+    public Queen(int color, int row, int col) {
+        super(color, row, col);
+        if(color == GamePanel.WHITE){
+            image = getImage("/piece_image/w_queen");
+        } else {
+            image = getImage("/piece_image/b_queen");
+        }
+    }
+    
+    public boolean canMove(int targetRow , int targetCol){
+        if(isWithinBoard(targetRow, targetCol) && isSamePosition(targetRow, targetCol) == false){
+            // horizontal or vertical movement
+            if(targetCol == preCol || targetRow == preRow){
+                if(isValidSquare(targetRow, targetCol) && limitMovement_straight(targetRow, targetCol)){
+                    return true;
+                }
+            }
+            // diagonal movement
+            if(Math.abs(targetRow - preRow) == Math.abs(targetCol - preCol)){
+                if(isValidSquare(targetRow, targetCol) && limitMovement_diagonal(targetRow, targetCol)){
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
